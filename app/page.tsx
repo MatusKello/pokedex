df6c5d8ef60a97d4f3f34c89a4ef40993009dea5
Matus Kello <m.kello1992@gmail.com>
2024-07-28 17:33:40 +0200

    edit on css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
-      excepturi quia inventore iure sit deserunt quae distinctio expedita animi
-      impedit magni, possimus, laboriosam delectus? Omnis, debitis aperiam! Sit,
-      eos quo?
-    </div>
-  );
+  return;
 }
